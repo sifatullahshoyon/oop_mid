1a7f3246a8497907dbbb470e348ee688fa8193a8
Sifat Ullah Shoyon <sifatullah.shoyon@gmail.com>
2026-07-02 01:32:28 +0600

    View All Student Work Done

--- student.py
+++ student.py
@@ -1,25 +1,44 @@
-class StudentDatabase:
-    # def __init__(self, student_id, name, department, is_enrolled) -> None:
-    #     self.student_id = student_id
-    #     self.name = name
-    #     self.department = department
-    #     self.enroll = is_enrolled
-
-    def __init__(self):
-        self.student_list = []
-
-    def add_student(self, student_id, name, department, is_enrolled):
+class Student:
+    def __init__(self, student_id, name, department, is_enrolled):
         self.student_id = student_id
         self.name = name
         self.department = department
-        self.enroll = is_enrolled
+        self.is_enrolled = is_enrolled
 
-class Student(StudentDatabase):
+
+class StudentDatabase:
     def __init__(self):
-        super().__init__()
+        self.student_list = []
 
-rahim = StudentDatabase(101, 'Rahim', 'Management', False)
-rahim.add_student(101, 'Rahim', 'Management', False)   
+    def add_student(self, student):
+        self.student_list.append(student)
+
+    def view_students(self):
+        for s in self.student_list:
+            print(s.student_id, s.name, s.department, s.is_enrolled)
+
+
+sd = StudentDatabase()
+
+s1 = Student(101, "Rahim", "Management", False)
+s2 = Student(102, "Karim", "CSE", True)
+s3 = Student(103, "Bosir", "CSE", True)
+s4 = Student(104, "Arosh Khan", "CSE", False)
+s5 = Student(105, "Munna", "Management", True)
+s6 = Student(106, "Arohi", "Tripoli", True)
+s7 = Student(107, "Sujon Ahmed", "Tripoli", False)
+s8 = Student(108, "Nazmul Hasan", "Physiology", True)
+
+sd.add_student(s1)
+sd.add_student(s2)
+sd.add_student(s3)
+sd.add_student(s4)
+sd.add_student(s5)
+sd.add_student(s6)
+sd.add_student(s7)
+sd.add_student(s8)
+
+
 
 while True:
     print('\n-------- Student Managment Menu --------\n')
@@ -33,7 +52,7 @@ while True:
 
     if option == 1:
         print("\n---------------------------------------------------------------------\n")
-        rahim.add_student()
+        sd.view_students()
     elif option == 2:
         pass
     elif option == 3:
